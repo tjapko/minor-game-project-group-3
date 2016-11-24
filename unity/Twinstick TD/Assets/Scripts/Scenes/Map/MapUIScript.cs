99f9f3365d209ae6b64f4c88091fd067e398cc47
4367480 <spamcheck7@hotmail.com>
2016-11-24 19:37:52 +0900

    Starting Merge currency

---
+++ unity/Twinstick TD/Assets/Scripts/Scenes/Map/MapUIScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class UI script
+/// Functions for the GameManager to implement UI 
+/// </summary>
+public class MapUIScript : MonoBehaviour {
+    private GameObject m_pausemenu;
+    private GameObject m_wavecontrol;
+    private GameObject m_constructionpanel;
+
+    // Initialization
+    void Start()
+    {
+        m_pausemenu = gameObject.transform.Find("PauseMenu").gameObject;
+        m_wavecontrol = gameObject.transform.Find("WaveControl").gameObject;
+        m_constructionpanel = gameObject.transform.Find("ConstructionPanel").gameObject;
+
+        m_pausemenu.SetActive(false);
+        m_wavecontrol.SetActive(false);
+        m_constructionpanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Show Pause menu
+    public void showPauseMenu()
+    {
+        m_pausemenu.SetActive(true);
+    }
+
+    // Show Pause menu
+    public void hidePauseMenu()
+    {
+        m_pausemenu.SetActive(false);
+    }
+
+    // Show Pause menu
+    public void showWaveControl()
+    {
+        m_wavecontrol.SetActive(true);
+    }
+
+    // Show Pause menu
+    public void hideWaveControl()
+    {
+        m_wavecontrol.SetActive(false);
+    }
+
+    // Show Pause menu
+    public void showConstructonPanel()
+    {
+        m_constructionpanel.SetActive(true);
+    }
+
+    // Show Pause menu
+    public void hideConstructonPanel()
+    {
+        m_constructionpanel.SetActive(false);
+    }
+
+}
