a6b5e7e787f85c2bc1f1ff29b3e71feabf61599e
Chahid Taaban <C.Taaban@student.tudelft.nl>
2016-11-25 15:47:44 +0100

    added Jorrit's scripts

--- unity/Twinstick TD/Assets/Scripts/Managers/EnemyManager.cs
+++ unity/Twinstick TD/Assets/Scripts/Managers/EnemyManager.cs
@@ -12,27 +12,37 @@ public class EnemyManager
     [HideInInspector] public Transform m_TargetPoint;   // Location of target
     [HideInInspector] public int m_EnemyNumber;         // Number of enemy
     [HideInInspector] public GameObject m_Instance;     // A reference to the instance of the enemy
-    [HideInInspector] public EnemyMovement m_Movement;  // Reference to enemy's movement script, used to disable and enable control.
+	[HideInInspector] public Unit m_Movement;  // Reference to enemy's movement script, used to disable and enable control.
     //private GameObject m_CanvasGameObject;            // Used to disable the world space UI during the Starting and Ending phases of each round.
 
     //Constructor
     public EnemyManager(GameObject instance, Transform spawnpoint, Transform target, int number)
     {
-        m_SpawnPoint = spawnpoint;
-        m_TargetPoint = target;
-        m_EnemyNumber = number;
-        m_Instance = instance;
-        m_Movement = m_Instance.GetComponent<EnemyMovement>();
-        m_Movement.m_targetlocation = target;
-        m_Movement.m_targetlocation = m_TargetPoint;
+        this.m_SpawnPoint = spawnpoint;
+        this.m_TargetPoint = target;
+        this.m_EnemyNumber = number;
+        this.m_Instance = instance;
+		this.m_Movement = m_Instance.GetComponent<Unit>();
+		m_Movement.target = m_TargetPoint;
     }
 
     //Setup
     public void Setup()
     {
-
     }
 
+//	public void OnTriggerEnter(Collider other){
+//	//if colide with base, damage base and set enemy to inactive
+//	if (other.gameObject.CompareTag ("Base")) {
+//		Debug.Log ("hoi");
+//		Rigidbody targetRigidbody = other.GetComponent<Rigidbody> ();
+//		if (targetRigidbody) {
+//			Basehealth basehealth = targetRigidbody.GetComponent<Basehealth> ();
+//			basehealth.TakeDamage (1f);
+//			m_Instance.SetActive (false);				
+//		}
+//	}
+//	}
 
     // Used during the phases of the game where the enemy shouldn't move
     public void DisableControl()
