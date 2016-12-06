eee7b9595def7727ee20d3c76fc306bb72edadac
Chahid Taaban <C.Taaban@student.tudelft.nl>
2016-12-06 18:11:06 +0100

    Added relative rotation (independent of the camera view)

--- unity/Twinstick TD/Assets/Scripts/Player/PlayerMovement.cs
+++ unity/Twinstick TD/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,27 +3,27 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public int m_PlayerNumber = 1;
+	// public variables 
+	[HideInInspector]public int m_PlayerNumber = 1; //Used to identify differnt players. Needs to be updated automatically when multiplayed-mode
 	[HideInInspector]public PlayerManager player;
-//	[HideInInspector]public int m_PlayerNumber = 0; // not used yet; can be used to identify the different players (later), each players needs different controls!
 	[HideInInspector]public Vector3 mouseposition;
-    //public float m_RotationSpeed = 1f; // not used!
 	public float m_MovementSpeed = 15f;
 	[HideInInspector]public bool useController;
 	public bool windowsAndXBOX=false; // mac is default 
-
+	// private variables 
 	private Rigidbody m_playerRigidbody;
-
 	private float m_MovementInputValueV;
 	private float m_MovementInputValueH;
-	//private Vector3 m_RotationInputM;
-
+//	private Vector3 m_RotationInputM; not used 
 	private int Floor;
 	private float camRayLength = 100f;
-
-	Vector3 movement;
+	private Vector3 movement;
+	private Vector3 playerDir;
 	private Transform cameraTransform;
-
+	private string m_MovementAxisNameV;
+	private string m_MovementAxisNameH;
+	private string m_RotationAxisNameV;
+	private string m_RotationAxisNameH;
 
 
 	// Initializes the Floormask 
@@ -35,18 +35,28 @@ public class PlayerMovement : MonoBehaviour {
 	// Initializes the player's Rigidbody
     private void Start()
     {
-        m_playerRigidbody = GetComponent<Rigidbody>(); 
+        m_playerRigidbody = GetComponent<Rigidbody>();
+		// input field for movement 
+		m_MovementAxisNameV = "Vertical_" + m_PlayerNumber; 
+		m_MovementAxisNameH = "Horizontal_" + m_PlayerNumber;
+		// input field for rotation, mac 
+		if (!windowsAndXBOX) {
+			m_RotationAxisNameH = "RightJoystickHorizontalMac_" + m_PlayerNumber;
+			m_RotationAxisNameV = "RightJoystickVerticalMac_" + m_PlayerNumber;
+		}
+		// input field for rotation, for using XBOX controller on windows 
+		else {
+			m_RotationAxisNameH = "RightJoystickHorizontalWindowsXBOX_" + m_PlayerNumber;
+			m_RotationAxisNameV = "RightJoystickVerticalWindowsXBOX_" + m_PlayerNumber;
+		}
+
     }
 
 	//Every physics step: Abstracting Vertical,Horizontal and mousePosition input and Updating player's position and rotation
 	private void FixedUpdate()
 	{
-		
-		string m_MovementAxisNameV = "Vertical_" + m_PlayerNumber; 
-		string m_MovementAxisNameH = "Horizontal_" + m_PlayerNumber;
-
         // Store the player's input.
-        m_MovementInputValueV = Input.GetAxisRaw(m_MovementAxisNameV);   // use Input.GetAxisRaw("Vertical") for instant reaction of the Vertical movement 
+        m_MovementInputValueV = Input.GetAxisRaw(m_MovementAxisNameV); // use Input.GetAxisRaw("Vertical") for instant reaction of the Vertical movement 
 		m_MovementInputValueH = Input.GetAxisRaw(m_MovementAxisNameH); // use Input.GetAxisRaw("Horizontal") for instant reaction of the Horizontal movement 
 		//m_RotationInputM = Input.mousePosition;
 
@@ -55,12 +65,9 @@ public class PlayerMovement : MonoBehaviour {
 		if (!useController) {
 			mouseTurn (); // Rotate the player with mouse
 		} 
-		else if (useController && !windowsAndXBOX) {
-			controllerTurnMac (); // Rotate the player with controller
+		else {
+			controllerTurn (); // Rotate the player with controller for both controllers (XBOX and PS3) on mac and PS3 on windows
 		} 
-		else if (useController && windowsAndXBOX) {
-			controllerTurnWindowsAndXBOX (); // Rotate the player with controller
-		}
 	}
 
 	// Adjust the position of the player based on the player's keyboard input.
@@ -81,7 +88,7 @@ public class PlayerMovement : MonoBehaviour {
 		movement = movement *  m_MovementSpeed * Time.deltaTime;
 
 		// Rotates (Vector3) movement 
-		movement = RotateWithView ();
+		movement = RotateMovementWithView ();
 
 		// adding movement to player's position
 		movement += m_playerRigidbody.position;
@@ -92,7 +99,7 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	// Rotate Vector3 movement with respect to the camera view
-	private Vector3 RotateWithView() 
+	private Vector3 RotateMovementWithView() 
 	{
 		// camera is moved (not fixed)
 		if (cameraTransform != null) 
@@ -115,7 +122,7 @@ public class PlayerMovement : MonoBehaviour {
 	private void mouseTurn()
 	{
 		// creating a ray from the camera to the mouseposition
-		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition); 
 
 		// a variable, which is true when the ray hits the floor 
 		RaycastHit floorHit;
@@ -136,28 +143,39 @@ public class PlayerMovement : MonoBehaviour {
 		}
 	}
 
-	// Adjust the rotation of the player based on the controller's right-joystick input for the PS3-controller (windows and mac) and XBOX-controller (mac)
-	private void controllerTurnMac() {
-		// horizontal & vertical movement, used GetAxis() istead of GetAxisRaw()
-		Vector3 playerDir = Vector3.right * Input.GetAxis ("RightJoystickHorizontalMac_"+ (m_PlayerNumber)) + 
-			Vector3.forward * -1 * Input.GetAxis ("RightJoystickVerticalMac_"+ (m_PlayerNumber));  
+	// Adjust the rotation of the player based on the controller's right-joystick input
+	private void controllerTurn() {
+		// horizontal & vertical rotation, used GetAxis() istead of GetAxisRaw()
+		playerDir = Vector3.right * Input.GetAxis (m_RotationAxisNameH) + 
+					Vector3.forward * -1 * Input.GetAxis (m_RotationAxisNameV);  
 
 		// check if player's input isn't zero, sp player is actually rotating 
 		if (playerDir.sqrMagnitude > 0.0f) { 
-			transform.rotation = Quaternion.LookRotation(playerDir, Vector3.up);
+			// Extra rotation to fix relative rotation axis
+			playerDir = RotateRotationtWithView();
+			// Rotate the player to the newRotation
+			Quaternion newRotation = Quaternion.LookRotation(playerDir);
+			m_playerRigidbody.MoveRotation(newRotation);
 		}
 	}
-
-	// Adjust the rotation of the player based on the controller's right-joystick input for the XBOX-controller (windows) 
-	private void controllerTurnWindowsAndXBOX() {
-		// horizontal & vertical movement, used GetAxis() istead of GetAxisRaw()
-		Vector3 playerDir = Vector3.right * Input.GetAxis ("RightJoystickHorizontalWindowsXBOX_"+ (m_PlayerNumber)) + 
-			Vector3.forward * -1 * Input.GetAxis ("RightJoystickVerticalWindowsXBOX_"+ (m_PlayerNumber));  
-
-		// check if player's input isn't zero, sp player is actually rotating 
-		if (playerDir.sqrMagnitude > 0.0f) { 
-			transform.rotation = Quaternion.LookRotation(playerDir, Vector3.up);
+		
+	// Rotate Vector3 movement with respect to the camera view
+	private Vector3 RotateRotationtWithView() {
+		// camera is moved (not fixed)
+		if (cameraTransform != null) 
+		{
+			// calculate the new rotated, right-oriented movement vector 
+			Vector3 dir = cameraTransform.TransformDirection (playerDir);
+			dir.y = 0f; // y-value is keeped zero
+			return dir.normalized * movement.magnitude; // same length as before ()
 		}
+		// camera is not moved (fixed)
+		else 
+		{
+			cameraTransform = Camera.main.transform; 
+			return playerDir; // just return playerDir (already right orientation)
+		}
+
 	}
 
 }
--- unity/Twinstick TD/Assets/Scripts/Player/SwitchControl.cs
+++ unity/Twinstick TD/Assets/Scripts/Player/SwitchControl.cs
@@ -3,25 +3,28 @@ using System.Collections;
 
 public class SwitchControl : MonoBehaviour {
 
-	public int m_PlayerNumber = 1;
+	// public variables 
+	[HideInInspector]public int m_PlayerNumber = 1;  //Used to identify differnt players. Needs to be updated automatically when multiplayed-mode
 	[HideInInspector]public PlayerManager player;
 	public PlayerMovement playerMovement; // PlayerMovement script for updating boolean useController 
 
 	// Update is called once per frame
 	void Update () {
-	
-		// detect mouse input, if so set boolean useController to false! 
+		// detect mouse input, if so set (boolean) useController to false! 
 		// mouseHovering for orientation 
-		if (Input.GetAxisRaw ("Mouse X_" + m_PlayerNumber) != 0.0f || Input.GetAxisRaw ("Mouse Y_" + m_PlayerNumber) != 0.0f) {
+		if (Input.GetAxisRaw ("Mouse X_" + m_PlayerNumber) != 0.0f || 
+			Input.GetAxisRaw ("Mouse Y_" + m_PlayerNumber) != 0.0f) {
 			playerMovement.useController = false;
 		}
 
 		// detect controller input 
 		// rightJoystick for orientation 
-		if (Input.GetAxisRaw ("RightJoystickHorizontalMac_"+ (m_PlayerNumber)) != 0.0f || Input.GetAxisRaw ("RightJoystickVerticalMac_"+ (m_PlayerNumber)) != 0.0f || 
-			Input.GetAxisRaw ("RightJoystickHorizontalWindowsXBOX_"+ (m_PlayerNumber)) != 0.0f || Input.GetAxisRaw ("RightJoystickVerticalWindowsXBOX_"+ (m_PlayerNumber)) != 0.0f ) {
+		if (Input.GetAxisRaw ("RightJoystickHorizontalMac_"+ (m_PlayerNumber)) != 0.0f || 
+			Input.GetAxisRaw ("RightJoystickVerticalMac_"+ (m_PlayerNumber)) != 0.0f || 
+			Input.GetAxisRaw ("RightJoystickHorizontalWindowsXBOX_"+ (m_PlayerNumber)) != 0.0f || 
+			Input.GetAxisRaw ("RightJoystickVerticalWindowsXBOX_"+ (m_PlayerNumber)) != 0.0f ) {
 			playerMovement.useController = true;
 		}
-
 	}
+
 }
