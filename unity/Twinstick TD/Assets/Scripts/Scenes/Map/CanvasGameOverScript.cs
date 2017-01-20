0e9747fb91c1fc1a4714fef300c7d0475a97caf1
Alex Lewis <ajlewis@live.nl>
2017-01-20 17:36:29 +0100

    Working UI

--- unity/Twinstick TD/Assets/Scripts/Scenes/Map/CanvasGameOverScript.cs
+++ unity/Twinstick TD/Assets/Scripts/Scenes/Map/CanvasGameOverScript.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
+
 public class CanvasGameOverScript : MonoBehaviour {
 
     //References to managers
@@ -15,6 +17,12 @@ public class CanvasGameOverScript : MonoBehaviour {
     //References to GameObject
     private GameObject m_gameovermenu;  //Reference to the game over menu
     private GameObject m_restartbutton; //Reference to the restart button
+	private GameObject m_HighScoreCanvas;
+	private string addScoreURL = "https://insyprojects.ewi.tudelft.nl/ewi3620tu3/addscore.php?"; //be sure to add a ? to your url
+	private string secretKey = "mySecretKey"; // Edit this value and make sure it's the same as the one stored on the server
+	public string[] items;
+
+	private HSController m_HSController;      //Reference to CanvasGameOverScript
 
     public void StartInitialization()
     {
@@ -26,6 +34,7 @@ public class CanvasGameOverScript : MonoBehaviour {
 
         //Set references to children
         m_gameovermenu = gameObject.transform.GetChild(0).gameObject;
+		m_HighScoreCanvas = gameObject.transform.GetChild(1).gameObject;
         m_restartbutton = m_gameovermenu.transform.GetChild(2).gameObject;
         //Activate game over menu
         m_gameovermenu.SetActive(true);
@@ -37,6 +46,28 @@ public class CanvasGameOverScript : MonoBehaviour {
     {
         m_gameovermenu.SetActive(status);
     }
+
+	// Back menu
+	public void GameoverMenu()
+	{
+		m_gameovermenu = gameObject.transform.GetChild(0).gameObject;
+		m_HighScoreCanvas = gameObject.transform.GetChild(1).gameObject;
+		m_gameovermenu.SetActive(true);
+		m_HighScoreCanvas.SetActive (false);
+	}
+
+	// Show/hide the score menu
+	public void showHighScore()
+	{
+		m_gameovermenu = gameObject.transform.GetChild(0).gameObject;
+		m_HighScoreCanvas = gameObject.transform.GetChild(1).gameObject;
+		m_HSController = m_HighScoreCanvas.GetComponent<HSController> ();
+		m_gameovermenu.SetActive(false);
+		m_HighScoreCanvas.SetActive (true);
+		m_HSController.StartInitialization();
+
+
+	}
 
     // Get the score of the player
     public void setScore()
@@ -52,7 +83,7 @@ public class CanvasGameOverScript : MonoBehaviour {
         {
             PlayerManager player = playerlist[i];
             score[i] = player.m_stats.getkills() * 10 + player.m_stats.getCurrency();
-            scoretext.text += "Player" + (i + 1) + " : " + score[i];
+            scoretext.text += "   Score : " + score[i];
         }
     }
 
@@ -64,9 +95,32 @@ public class CanvasGameOverScript : MonoBehaviour {
         NextScene(sceneindex);
     }
 
+
     // Load next scene
     public void NextScene(int sceneindex)
     {
         SceneManager.LoadScene(sceneindex);
     }
+
+	// remember to use StartCoroutine when calling this function!
+	IEnumerator PostScores(string Name, int Score)
+	{
+		print ("recieved");
+		//This connects to a server side php script that will add the name and score to a MySQL DB.
+		// Supply it with a string representing the players name and the players score.
+		var hash=Md5.Md5Sum(Name + Score + secretKey);
+
+		string post_url = addScoreURL + "Name=" + WWW.EscapeURL(Name) + "&Score=" + Score + "&hash=" + hash;
+		print (post_url);
+		// Post the URL to the site and create a download object to get the result.
+		WWW hs_post = new WWW(post_url);
+		yield return hs_post; // Wait until the download is done
+		string itemsDataString = hs_post.text;
+		print (itemsDataString);
+		items = itemsDataString.Split(';');
+		if (hs_post.error != null)
+		{
+			print("There was an error posting the high score: " + hs_post.error);
+		}
+	}
 }
--- unity/Twinstick TD/Assets/Scripts/WebData/HSController.cs
+++ unity/Twinstick TD/Assets/Scripts/WebData/HSController.cs
@@ -6,13 +6,13 @@ public class HSController : MonoBehaviour
 {
 	
     private string secretKey = "mySecretKey"; // Edit this value and make sure it's the same as the one stored on the server
-    public string addScoreURL = "https://insyprojects.ewi.tudelft.nl/ewi3620tu3/addscore.php?"; //be sure to add a ? to your url
+    
     public string highscoreURL = "https://insyprojects.ewi.tudelft.nl/ewi3620tu3/HSData.php";
 	public string[] items;
 	private GameObject m_HighScoreMenu; 
 	private GameObject m_backbutton;
 
-    void Start()
+	public void StartInitialization()
     {
 		//Set references to children
 		m_HighScoreMenu = gameObject.transform.GetChild(0).gameObject;
@@ -24,34 +24,7 @@ public class HSController : MonoBehaviour
 
     }
  
-
-	public void showHighScoreMenu(bool status)
-	{
-		m_HighScoreMenu.SetActive(status);
-	}
-
-    // remember to use StartCoroutine when calling this function!
-    IEnumerator PostScores(string Name, int Score)
-    {
-		print ("recieved");
-        //This connects to a server side php script that will add the name and score to a MySQL DB.
-        // Supply it with a string representing the players name and the players score.
-		var hash=Md5.Md5Sum(Name + Score + secretKey);
-
-		string post_url = addScoreURL + "Name=" + WWW.EscapeURL(Name) + "&Score=" + Score + "&hash=" + hash;
-		print (post_url);
-        // Post the URL to the site and create a download object to get the result.
-		WWW hs_post = new WWW(post_url);
-        yield return hs_post; // Wait until the download is done
-		string itemsDataString = hs_post.text;
-		print (itemsDataString);
-		items = itemsDataString.Split(';');
-        if (hs_post.error != null)
-        {
-            print("There was an error posting the high score: " + hs_post.error);
-        }
-    }
- 
+	 
     // Get the scores from the MySQL DB to display in a GUIText.
     // remember to use StartCoroutine when calling this function!
     IEnumerator GetScores()
@@ -78,8 +51,6 @@ public class HSController : MonoBehaviour
 	}
 
 }
-
-
 
 
 
