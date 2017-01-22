33759eb8cf03cd5391956a12e2f6dec82b81d7c2
4367480 <spamcheck7@hotmail.com>
2017-01-23 00:49:12 +0900

    Fix:Escape button uprade base

--- unity/Twinstick TD/Assets/Scripts/Managers/GameManager.cs
+++ unity/Twinstick TD/Assets/Scripts/Managers/GameManager.cs
@@ -94,6 +94,9 @@ public class GameManager : MonoBehaviour
                 //Check if players are constructing
                 else if(m_players.checkConstruction()){
 
+                } else if (m_uiscript.go_CanvasBaseUpgrade.activeSelf)
+                {
+                    m_uiscript.go_CanvasBaseUpgrade.SetActive(false);
                 }
                 else
                 {
