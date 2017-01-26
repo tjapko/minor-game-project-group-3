addfddad63bf244068486f46eab7739fdc545169
Chahid Taaban <C.Taaban@student.tudelft.nl>
2017-01-26 12:59:28 +0100

    zoom function in construction mode

--- unity/Twinstick TD/Assets/zoomControl.cs
+++ unity/Twinstick TD/Assets/zoomControl.cs
@@ -5,11 +5,14 @@ public class zoomControl : MonoBehaviour {
 
 	private Camera m_constructionCam;
 	private Vector3 m_CamPos;
+	private float camRayLength = 100f;
+	private int Floor;   
 
 	// Use this for initialization
 	void Start () {
 		m_constructionCam = GetComponent<Camera> ();
 		m_CamPos = m_constructionCam.transform.position;
+		Floor = LayerMask.GetMask ("mouseFloor");
 	}
 	
 	// Update is called once per frame
@@ -18,15 +21,41 @@ public class zoomControl : MonoBehaviour {
 		float scroll = Input.GetAxis("Mouse ScrollWheel");
 		if (scroll > 0)
 		{
-			m_constructionCam.orthographicSize = 30;
-//			m_constructionCam.transform.position = new Vector3 (Input.mousePosition.x, m_CamPos.y, Input.mousePosition.z);
+			if (m_constructionCam.orthographicSize > 40) {
+				m_constructionCam.orthographicSize -= 20;
+				m_constructionCam.transform.position = mousePos ();
+			}
+//			m_constructionCam.transform.position = mousePos ();
 		}
 		
 		if (scroll < 0)
 		{
-			m_constructionCam.transform.position = m_CamPos;
-			m_constructionCam.orthographicSize = 70;
+			if (m_constructionCam.orthographicSize <= 60) {
+				m_constructionCam.orthographicSize = 70;
+				m_constructionCam.transform.position = mousePos ();
+			} else {
+				m_constructionCam.transform.position = m_CamPos;
+				m_constructionCam.orthographicSize = 70;
+			}
 		}
-	
+	}
+
+	// Adjust the rotation of the player based on the mousePosition input.
+	private Vector3 mousePos()
+	{
+		// creating a ray from the camera to the mouseposition
+		Ray camRay = m_constructionCam.ScreenPointToRay(Input.mousePosition);
+
+		// a variable, which is true when the ray hits the floor 
+		RaycastHit floorHit;
+
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, Floor)) // checking if the ray hits the floor 
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position; // creating a vector from the player to the mousePosition
+			playerToMouse.y = 0f; // because it's a projection on the x-z-plane
+			playerToMouse = transform.position + playerToMouse;  //Used for dropping objects
+			return playerToMouse;
+		}
+		return Vector3.zero;
 	}
 }
